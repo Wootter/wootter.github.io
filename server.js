780d87dedc53b490a83507cd7207061a701f7542
Wout <woudeelen@gmail.com>
2025-02-21 11:22:37 +0100

    changed html directory in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 const path = require('path');
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
